09814ff5ee0ecbe11987d9efc7ecdceabc093222
Moisessantos45 <120003425+Moisessantos45@users.noreply.github.com>
2023-10-03 18:26:23 -0600

    el index actualizado

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+// const root = document.getElementById("root");
+// const reactRoot = createRoot(root);
+// reactRoot.render(<HeroesApp />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>¿
+        <RouterProvider router={router} />¿
+    </React.StrictMode>
+)
